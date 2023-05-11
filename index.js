0b88c4915d9f544602fcde2ea81b143f5187e376
Praveen <praveentuf15@gmail.com>
2023-05-11 22:55:44 +0530

    changes

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 //document.getElementById("count-el").innerText = 5;
 
-let firstBatch = 5;
-let secondBatch = 7;
-
-let count = firstBatch + secondBatch;
-
-console.log(count);
+let countEl = document.getElementById("count-el");
+let count = 0;
